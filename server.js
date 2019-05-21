ef3e93a85736d24287a9ca94cf9c2693e665ce94
sabdi21 <sabdi21@hotmail.com>
2019-05-21 13:48:36 -0700

    almost done with todays lab

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const superagent = require('superagent');
 
 // Application Setup
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 // Application Middleware
 app.use(express.urlencoded({extended:true}));
@@ -18,20 +18,38 @@ app.set('view engine', 'ejs');
 // API Routes
 // Renders the search form
 app.get('/', newSearch);
-
+app.get('/books', getBooks);
 
 // Creates a new search to the Google Books API
 app.post('/searches', createSearch);
 
-
+//ERROR HANDLER
+function handleError(err, res) {
+  console.error(err);
+  if (res) res.status(500).send('Sorry, something went wrong')
+}
 
 // HELPER FUNCTIONS
-// Only show part of this to get students started
-function Book(info) {
+function Books(info) {
   const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+  const placeholder = 'default';
+  this.image = placeholderImage;
+  this.title = placeholder
 
-  this.title = info.title || 'No title available';
+}
 
+function getBooks(request, response) {
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${this.query}&key=${process.env.BOOKS_API_KEY}`;
+  superagent.get(url)
+    .then(result => {
+      const books = result.body.books.map(bookData => {
+        const info = new Books(bookData);
+        return info;
+      });
+
+      response.send(books);
+    })
+    .catch(error => handleError(error, response));
 }
 
 // Note that .ejs file extension is not required
@@ -49,15 +67,19 @@ function createSearch(request, response) {
 
   if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
   if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
+  if (request.body.search[1] === 'genre') { url += `+ingenre:${request.body.search[0]}`; }
 
   superagent.get(url)
-    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
-    .then(results => response.render('pages/searches/show', {searchResults: results}));
-  // how will we handle errors?
-}
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Books(bookResult.volumeInfo)))
+    .then(books => response.render('pages/searches/show', {searchResults: books}));
 
+  // how will we handle errors?
+
+}
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 // Catch-all
-app.get('*', (request, response) => response.status(404).send('This route does not exist'));
+app.get('*', (request, response) => response.render(`pages/error`));
+
+//app.get('*', (request, response) => response.status(404).send('This route does not exist'));
